aa5fb7dbac1928995585420cae464e541e18bc6b
joshuaroytino <joshuaroytino@gmail.com>
2022-08-20 13:14:04 +0800

    Revert "Add ability to authenticate in the web app."
    This reverts commit aa28e4d66d48875ede3e7105bbfa813dd6359ba3.
    
    # Conflicts:
    #	app/Providers/FortifyServiceProvider.php
    #	routes/api.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,11 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    protected function password(): \Attribute
-    {
-        return \Attribute::make(
-            set: fn ($value) => bcrypt($value),
-        );
-    }
 }
